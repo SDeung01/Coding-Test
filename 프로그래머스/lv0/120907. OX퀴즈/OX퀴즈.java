c7ff3d4daeda8b76f318fdf27c93de8c535807f6
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-09-27 16:22:56 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -3,17 +3,9 @@ class Solution {
         String[] result = new String[quiz.length];
         for(int i = 0; i < quiz.length; i++){
             String[] exp = quiz[i].split(" ");
-            boolean ox = false;
-            switch(exp[1]){
-                case "+":
-                    ox = Integer.parseInt(exp[0]) + Integer.parseInt(exp[2]) == Integer.parseInt(exp[4]);
-                    result[i] = ox ? "O" : "X";
-                    break;
-                case "-":
-                    ox = Integer.parseInt(exp[0]) - Integer.parseInt(exp[2]) == Integer.parseInt(exp[4]);
-                    result[i] = ox ? "O" : "X";
-                    break;
-            }
+            int operator = exp[1].equals("+") ? 1 : -1;
+            boolean ox = Integer.parseInt(exp[0]) + operator * Integer.parseInt(exp[2]) == Integer.parseInt(exp[4]);
+            result[i] = ox ? "O" : "X";
         }
         return result;
     }
--- 프로그래머스/lv0/120907. OX퀴즈/README.md
+++ 프로그래머스/lv0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907?language=java) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.10 ms
+메모리: 78.9 MB, 시간: 0.10 ms
 
 ### 구분
 

878f1c53fe832d5d10d660772e538c62018054c1
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-09-27 12:33:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int bigDenom = Math.max(denom1,denom2);
-        int smallDenom = Math.min(denom1,denom2);
         
-        int bigNumer = bigDenom == denom1 ? numer1 : numer2;
-        int smallNumer = smallDenom == denom1 ? numer1 : numer2;
-        
-        int sumDenom = bigDenom * smallDenom;
-        int sumNumer = (bigNumer * smallDenom) + (smallNumer * bigDenom);
+        int sumDenom = denom1 * denom2;
+        int sumNumer = (numer1 * denom2) + (numer2 * denom1);
         
 
-        for(int i = 0; i < 10; i++){
-            for(int j = 2; j <= 1000; j++){
+            for(int j = 1000; j >= 2; j--){
                 if(sumNumer%j == 0 && sumDenom%j == 0){
                     sumNumer /= j;
                     sumDenom /= j;    
                 } 
             }
-        }
         int[] answer = {sumNumer, sumDenom};
         return answer;
     }

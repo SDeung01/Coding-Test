1dee276fb941678d5af8510473678313a66b8059
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-09-27 17:52:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 5.61 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] array) {
+        // 중복값 확인을 위해 배열 -> 리스트
+        List<Integer> arrList = IntStream.of(array).boxed().collect(Collectors.toList());
+        List<Integer> freqList = new ArrayList<>();
+        
+        // 확인한 중복값을 새로 만든 중복값리스트에 저장
+        for(int i = 0; i < arrList.size(); i++){
+            freqList.add(Collections.frequency(arrList,arrList.get(i)));
+        }
+        
+        int maxFreq = freqList.stream().mapToInt(i -> i).max().orElse(0);
+        int maxFreqFreq = Collections.frequency(freqList, maxFreq);
+        int maxIndex = freqList.indexOf(maxFreq);
+        int mode = arrList.get(maxIndex);
+
+        // 최빈값이 하나 이상인지 확인하여(중복값의 중복값과 중복값을 비교) -1 or 최빈값 출력
+        return maxFreq == maxFreqFreq ? mode : -1;
+    }
+}

9cef8fd48dc66ad7cf6c31f2e01f81936053f55f
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-13 00:01:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.33 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -9,7 +9,7 @@ class Solution {
     }
 
     // 리포트를 가져와 중복 신고 접수를 없애고 이용자 id와 신고된 id를 구분하여 배열로 저장, 그것을 요소로 하는 리스트를 반환
-    private ArrayList<String[]> reportUser(String[] report) {
+    public ArrayList<String[]> reportUser(String[] report) {
         HashSet<String> reportSet = new HashSet<>(Arrays.asList(report));
         ArrayList<String[]> reportList = new ArrayList<>();
 
@@ -21,10 +21,11 @@ class Solution {
     }
 
     //신고된 id 중 정지 기준치를 넘긴 id를 리스트로 반환 
-    private ArrayList<String> banedUser(ArrayList<String[]> reportList, int k) {
+    public ArrayList<String> banedUser(ArrayList<String[]> reportList, int k) {
         ArrayList<String> banedId = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
 
+        // Map (신고된 id, 신고된 횟수) 로 저장
         for (String[] reporter : reportList) {
             map.put(reporter[1], map.getOrDefault(reporter[1], 0) + 1);
         }
@@ -38,7 +39,7 @@ class Solution {
     }
 
     // 이용자의 신고내역 중 정지된 id가 있으면 메일을 발송하고 그 횟수를 배열로 반환
-    private int[] sendingMail(String[] id_list, ArrayList<String[]> reportList, ArrayList<String> banedList) {
+    public int[] sendingMail(String[] id_list, ArrayList<String[]> reportList, ArrayList<String> banedList) {
         int[] sentMail = new int[id_list.length];
         for (int i = 0; i < id_list.length; i++) {
             String userId = id_list[i];

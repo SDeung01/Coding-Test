5e55c3f95955a8fb35b6780a50ec5c069949ce1b
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-18 22:24:45 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=java) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.13 ms
+메모리: 68.9 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 20:0:8
+2023년 10월 3일 22:24:42
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,36 +1,18 @@
-import java.util.Stack;
-
 class Solution {
     public int solution(int[] ingredient) {
-        int count = 0;
-        Stack<Integer> bugerStk = new Stack<>();
-
-        for(int ing_num : ingredient){
-            bugerStk.push(ing_num);
-            int lastIndex = bugerStk.size() - 1;
-            if(bugerStk.size() >= 4){
-                boolean check = canMake(bugerStk, lastIndex);
-                if(check) {
-                    makeBuger(bugerStk);
-                    count++;
-                }
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
             }
         }
-        return count;
+        return answer;
     }
-
-    private boolean canMake(Stack<Integer> bugerStk, int lastIndex){
-        int layer4 = (int) bugerStk.get(lastIndex);
-        int layer3 = (int) bugerStk.get(lastIndex - 1);
-        int layer2 = (int) bugerStk.get(lastIndex - 2);
-        int layer1 = (int) bugerStk.get(lastIndex - 3);
-        return layer1 == 1 && layer2 == 2 && layer3 == 3 && layer4 == 1;
-    }
-
-    private void makeBuger(Stack<Integer> bugerStk) {
-            for(int i = 0; i < 4; i++){
-                bugerStk.pop();
-            }
-        }
-
 }

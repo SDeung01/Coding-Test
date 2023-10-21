74242ce306e4cc2d99920c81755c1cf11e8b5195
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-21 21:54:36 +0900

    [level 1] Title: 문자열 나누기, Time: 0.50 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=java) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 31.36 ms
+메모리: 76.8 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 6일 19:23:47
+2023년 10월 6일 21:54:33
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,27 +1,25 @@
 class Solution {
+
     public int solution(String s) {
-        int counter = 0;
-        String crop = s;
-        String str = "";
-        while (!str.equals(crop) && !crop.equals("")){
-            str = crop;
-            crop = cropStr(str);
-            counter++;
+        char prev = '1';
+        int same = 0, different = 0, answer = 0;
+        for (char c : s.toCharArray()) {
+            if (prev == '1') {
+                prev = c;
+                same++;
+                answer++;
+            } else if (prev == c) {
+                same++;
+            } else {
+                different++;
+            }
+
+            if (same == different) {
+                prev = '1';
+                same = 0; different = 0;
+            }
         }
-        
-        return counter;
-    }
-    
-    private String cropStr(String str){
-        char firstCh = str.charAt(0);
-        int sameCnt = 0;
-        int diffCnt = 0;
-        for(char ch : str.toCharArray()){
-            if(ch == firstCh) sameCnt++;
-            else diffCnt++;
-            if(sameCnt == diffCnt)
-                return str.substring(sameCnt + diffCnt);
-        }
-        return str;
+
+        return answer;
     }
 }

e5ebb22b359ee39fbc15ee8ebdcd9a3ecd076ed8
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-22 23:53:20 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 17.28 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] temp = new int[arr.length];
+        Arrays.fill(temp, Integer.MAX_VALUE);
+        int idx = 0;
+        for(int i = 0; i < arr.length; i++){
+            int prevNum = i == 0 ? Integer.MAX_VALUE : temp[idx - 1];
+            if(prevNum != arr[i]){
+                temp[idx] = arr[i];
+                idx++;
+            }
+        }
+        
+        return Arrays.copyOfRange(temp, 0, idx);
+    }
+}

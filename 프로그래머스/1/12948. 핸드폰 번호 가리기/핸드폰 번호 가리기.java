0b17df798f60bc7812715393153b997de84d1791
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-27 00:27:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,18 +1,9 @@
-// class Solution {
-//     public String solution(String phone_number) {
-//         int cropIdx = phone_number.length()-4;
-//         String stars = "*".repeat(cropIdx);
-//         return stars + phone_number.substring(cropIdx);
-//     }
-// }
-
 class Solution {
-    public String solution(String phone_number) {
-        int cropIdx = phone_number.length()-4;
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < cropIdx; i++){
-            sb.append("*");
-        }
-        return sb.append(phone_number.substring(cropIdx)).toString();
-    }
+  public String solution(String phone_number) {
+     char[] ch = phone_number.toCharArray();
+     for(int i = 0; i < ch.length - 4; i ++){
+         ch[i] = '*';
+     }
+     return String.valueOf(ch);
+  }
 }

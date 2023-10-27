4e0838b92c92d86e869b1143a3921f95782bd054
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-10-28 00:30:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i = 0; i < n; i++){
+            String map1 = Integer.toBinaryString(arr1[i]);
+            String map2 = Integer.toBinaryString(arr2[i]);
+            long longMap1 = Long.parseLong(map1);
+            long longMap2 = Long.parseLong(map2);
+            String specifier = "%0"+n+"d";
+            String map = String.format(specifier, longMap1 + longMap2);
+            map = map.replace("0"," ");
+            map = map.replace("1","#");
+            map = map.replace("2","#");
+            answer[i] = map;
+        }
+        return answer;
+    }
+}

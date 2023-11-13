5aa574f241b1437865f6cfcd8d8368591afb8ab9
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-11-13 23:26:51 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 3.98 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -9,6 +9,8 @@ class Solution {
             //group2
             for(int j = 0; j < cardArr.length; j++){
                 int[] leftCards = cardArr.clone();
+                // 1번 그룹에서 선택한 상자는 제외했기 때문에 2번 그룹에서 뽑을 일이 없다.
+                // 따라서 -1 일 경우 continue
                 if(leftCards[j] == -1) { continue; }
                 int score2 = group(leftCards, j);
                 int newScore = score1 * score2;
@@ -21,6 +23,8 @@ class Solution {
         return highScore;
     }
 
+    // 고른 상자 안의 카드에 적힌 숫자에 따라 다음 뽑을 상자를 고름
+    // 연 상자에는 표시(-1)를 하고 이미 연 상자를 다 시 고를 때 까지 반복하며 그때까지의 횟수를 반환
     private int group (int[] cardArr, int startIdx){
         int selectIdx = startIdx;
         int cnt = 0;

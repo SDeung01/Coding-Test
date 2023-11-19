0ad6f225f72fd4b581b528795b841e06d70a91d7
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-11-19 19:26:28 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n, int a, int b) {
         int round = 1;
-        
+        a -= 1;
+        b -= 1;
         while(true){
-            if((a - 1) / 2 == (b - 1) / 2) break;
-            a = (a - 1) / 2 + 1;
-            b = (b - 1) / 2 + 1;
+            if(a / 2 == b / 2) break;
+            a = a / 2;
+            b = b / 2;
             round++;
         }
 

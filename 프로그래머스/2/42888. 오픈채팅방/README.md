4ead7951af44e09f8a7364c35c432e0dcdb09053
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-11-19 21:55:31 +0900

    [level 2] Title: 오픈채팅방, Time: 93.85 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=java) 
 
 ### 성능 요약
 
-메모리: 161 MB, 시간: 117.52 ms
+메모리: 148 MB, 시간: 93.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 20:43:13
+2023년 11월 0일 21:55:28
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,38 +2,41 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {        
-        Map<String, String> logMap = new HashMap<>();
-        List<String[]> messageList = new ArrayList<>();
+        Map<String, String> userMap = new HashMap<>();
+        List<String[]> commandList = new ArrayList<>();
         
         for(String log : record){
             String[] logArr = log.split(" ");
             switch(logArr[0]){
                 case "Enter":
-                    logMap.put(logArr[1],logArr[2]);
-                    messageList.add(new String[] {logArr[1], makeMessage(logArr)});
+                    userMap.put(logArr[1],logArr[2]);
+                    commandList.add(new String[] {logArr[1], logArr[0]});
                     break;
                 case "Leave":
-                    messageList.add(new String[] {logArr[1], makeMessage(logArr)});
+                    commandList.add(new String[] {logArr[1], logArr[0]});
                     break;
                 case "Change":
-                    logMap.put(logArr[1],logArr[2]);
+                    userMap.put(logArr[1],logArr[2]);
             }
         }
         
-        String[] messageArr = new String[messageList.size()];
-        for(int i = 0; i < messageList.size(); i++){
-            String[] message = messageList.get(i);
-            messageArr[i] = message[1].replace(message[0], logMap.get(message[0]));
+        String[] messages = new String[commandList.size()];
+        for(int i = 0; i < commandList.size(); i++){
+            String[] commandArr = commandList.get(i);
+            String userId = commandArr[0];
+            String command = commandArr[1];
+            String nickName = userMap.get(userId);
+            messages[i] = makeMessage(command, nickName);
         }
         
-        return messageArr;
+        return messages;
     }
     
-    private String makeMessage(String[] logArr) {
-        if(logArr[0].equals("Enter")){
-            return logArr[1] + "님이 들어왔습니다.";
+    private String makeMessage(String command, String nickName) {
+        if(command.equals("Enter")){
+            return nickName + "님이 들어왔습니다.";
         }else {
-            return logArr[1] + "님이 나갔습니다.";
+            return nickName + "님이 나갔습니다.";
         }
     }
 }

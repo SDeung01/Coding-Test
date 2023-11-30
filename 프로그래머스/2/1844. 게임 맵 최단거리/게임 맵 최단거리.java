f8bfed51378521edd5afbad7bad5ddb922ad2f00
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2023-11-30 22:23:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.27 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,41 +6,44 @@ class Solution {
     class Point {
         private int x;
         private int y;
-        private int distance;
 
-        public Point(int x, int y, int distance) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.distance = distance;
         }
     }
 
     public int solution(int[][] maps) {
         int xLength = maps[0].length;
         int yLength = maps.length;
+        int x = 0, y = 0;
         // 탐색지 주변 상하좌우
         int[] aroundX = {0, 0, -1, 1};
         int[] aroundY = {-1, 1, 0, 0};
+        int cnt = 0;
         boolean[][] visited = new boolean[yLength][xLength];
         Queue<Point> roadQ = new LinkedList<>();
-        roadQ.offer(new Point(0, 0, 1));
-        visited[0][0] = true;
+        roadQ.offer(new Point(x, y));
+        visited[y][x] = true;
 
         while(!roadQ.isEmpty()){
-            Point road = roadQ.poll();
-            int distance = road.distance;
-            for(int j = 0; j < 4; j++){
-                int x = road.x + aroundX[j];
-                int y = road.y + aroundY[j];
-                if(x == xLength -1 && y == yLength -1) return distance+1;
-                if(x >= 0 && x < xLength && y >= 0 && y < yLength){
-                    if(maps[y][x] == 0 || visited[y][x]) continue;
-                    visited[y][x] = true;
-                    roadQ.offer(new Point(x, y, distance +1));
+            cnt++;
+            int size = roadQ.size();
+            for(int i = 0; i < size; i++){
+                Point road = roadQ.poll();
+                for(int j = 0; j < 4; j++){
+                    x = road.x + aroundX[j];
+                    y = road.y + aroundY[j];
+                    if(x == xLength -1 && y == yLength -1) return ++cnt;
+                    if(x >= 0 && x < xLength && y >= 0 && y < yLength){
+                        if(maps[y][x] == 0 || visited[y][x]) continue;
+                        visited[y][x] = true;
+                        roadQ.offer(new Point(x,y));
+                    }
                 }
             }
+
         }
-        
         return -1;
     }
     

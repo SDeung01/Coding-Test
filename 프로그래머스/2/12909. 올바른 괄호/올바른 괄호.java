3cafb2f0f4c416f09b65c1bed3c683b6ebf78aef
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2024-06-26 17:48:58 +0900

    [level 2] Title: 올바른 괄호, Time: 20.77 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -15,26 +15,5 @@ class Solution {
         }
         
         return bracketStk.size() == 0 ? true : false;
-        
-//         int shift = 0;
-//         int left = 0, right = 0;
-//         char prevBracket = '(';
-
-//         if(!s.startsWith("(") || !s.endsWith(")")) { return false; }
-//         else {
-//             for(int i = 0; i < s.length(); i++){
-//                 char nowBracket = s.charAt(i);
-//                 if(nowBracket == '(') left++;
-//                 else if(nowBracket == ')') right++;
-//                 if(nowBracket != prevBracket){
-//                     shift++;
-//                     if(shift % 2 == 0 && left != right+1){
-//                         return false;
-//                     }
-//                 }
-//                 prevBracket = nowBracket;
-//             }
-//         }
-
     }
 }

c8a83a01fc05bebf876d1643462caa1a9e403988
SDeung01 <145359737+SDeung01@users.noreply.github.com>
2024-06-28 09:08:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.22 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] strArr = s.split(" ");
+        int[] numArr = new int[strArr.length];
+        
+        for(int i = 0; i < strArr.length; i++){
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+        
+        Arrays.sort(numArr);
+        
+        return numArr[0] + " " + numArr[numArr.length -1];
+    }
+}
